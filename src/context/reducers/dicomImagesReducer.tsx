f26ef970877578b543019ddab8c0c5daa7f97655
janakitti <janakittiratana@gmail.com>
2021-02-02 10:36:25 -0500

    Create PACS response interfaces

--- src/context/reducers/dicomImagesReducer.tsx
+++ src/context/reducers/dicomImagesReducer.tsx
@@ -17,6 +17,52 @@ export interface DcmImage {
   Modality: string
 }
 
+export interface PACSResponseProperty {
+  tag: number;
+  value: string;
+  label: string;
+}
+
+export interface PACSResponseNumericProperty {
+  tag: number;
+  value: number;
+  label: string;
+}
+
+export interface PACSBaseResponse {
+  uid: PACSResponseNumericProperty;
+  SpecificCharacterSet: PACSResponseProperty;
+  StudyDate: PACSResponseProperty;
+  SeriesDate: PACSResponseProperty;
+  AccessionNumber: PACSResponseProperty;
+  QueryRetrieveLevel: PACSResponseProperty;
+  RetrieveAETitle: PACSResponseProperty;
+  Modality: PACSResponseProperty;
+  ModalitiesInStudy: PACSResponseProperty;
+  StudyDescription: PACSResponseProperty;
+  SeriesDescription: PACSResponseProperty;
+  PatientName: PACSResponseProperty;
+  PatientID: PACSResponseProperty;
+  PatientBirthDate: PACSResponseProperty;
+  PatientSex: PACSResponseProperty;
+  PatientAge: PACSResponseNumericProperty;
+  StudyInstanceUID: PACSResponseProperty;
+  SeriesInstanceUID: PACSResponseProperty;
+  InstanceNumber: PACSResponseProperty;
+  NumberOfSeriesRelatedInstances: PACSResponseProperty;
+  PerformedStationAETitle: PACSResponseProperty;
+}
+
+export interface PACSSeries extends PACSBaseResponse {
+  label: PACSResponseProperty;
+  command: PACSResponseProperty;
+  status: PACSResponseProperty;
+}
+
+export interface PACSMainResponse extends PACSBaseResponse {
+  series: PACSSeries[];
+}
+
 export type IDcmImagesState = DcmImage[];
 
 export const initialIDcmImagesState: IDcmImagesState = []
