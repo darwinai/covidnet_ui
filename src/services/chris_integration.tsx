22390960cc4c03958572d18fa5d65ed6c894a06e
Colin Wu <colin.y.wu@gmail.com>
2021-02-11 17:19:36 -0500

    Removed console log statement

--- src/services/chris_integration.tsx
+++ src/services/chris_integration.tsx
@@ -57,7 +57,6 @@ export const pollingBackend = async (pluginInstance: PluginInstance): Promise<Ba
   if (waitInterval > maxWaitInterval) {
     return { error: new Error('terminated due to timeout'), ...result }
   } else if ([PluginPollStatus.CANCELLED, PluginPollStatus.ERROR].includes(res.data.status)) {
-    console.log(res);
     return { error: new Error(`exited with status '${res.data.status}'`), ...result }
   } else {
     return result;
