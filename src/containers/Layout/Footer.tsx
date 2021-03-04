95cbc38c5f8c7f359e867dc5541b3a208e69ea6b
Colin Wu <colin.y.wu@gmail.com>
2021-03-04 14:00:29 -0500

    Edited scss formatting, spacing, naming convention, and removed redundant code

--- src/containers/Layout/Footer.tsx
+++ src/containers/Layout/Footer.tsx
@@ -16,18 +16,18 @@ import {
         <React.Fragment>
             <PageHeaderTools>
                 <PageHeaderToolsItem>
-                    <p className="footerText">An open-source neural network for disease detection created by 
-                    <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer"> DarwinAI</a>, 
-                    <a href="https://www.redhat.com/en" target="_blank" rel="noopener noreferrer"> RedHat</a>, and 
-                    <a href="https://www.childrenshospital.org/" target="_blank" rel="noopener noreferrer"> Boston Children's Hospital</a></p>
+                    <p className="footer-text">An open-source neural network for disease detection created by&nbsp;
+                    <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer">DarwinAI</a>,&nbsp;
+                    <a href="https://www.redhat.com/en" target="_blank" rel="noopener noreferrer">RedHat</a>, and&nbsp;
+                    <a href="https://www.childrenshospital.org/" target="_blank" rel="noopener noreferrer">Boston Children's Hospital</a></p>
                 </PageHeaderToolsItem>
             </PageHeaderTools>
             <PageHeaderTools>
-                <PageHeaderToolsItem className="footerLink">
-                    <p className="footerText">Find the Covid-Net project <a href="https://github.com/darwinai" target="_blank" rel="noopener noreferrer">here:</a></p>
-                    <a href="https://github.com/darwinai" target="_blank" rel="noopener noreferrer"><img src={github} className="footerLogo" alt="darwinAI"/></a>
-                    <p className="footerText">And the ChRIS project <a href="http://chrisproject.org/" target="_blank" rel="noopener noreferrer">here:</a></p>
-                    <a href="http://chrisproject.org/" target="_blank" rel="noopener noreferrer"><img src={chris} className="footerLogo" alt="ChRIS"/></a>
+                <PageHeaderToolsItem className="footer-link">
+                    <p className="footer-text">Find the Covid-Net project&nbsp;<a href="https://github.com/darwinai" target="_blank" rel="noopener noreferrer">here:</a></p>
+                    <a href="https://github.com/darwinai" target="_blank" rel="noopener noreferrer"><img src={github} className="footer-logo" alt="darwinAI"/></a>
+                    <p className="footer-text">And the ChRIS project&nbsp;<a href="http://chrisproject.org/" target="_blank" rel="noopener noreferrer">here:</a></p>
+                    <a href="http://chrisproject.org/" target="_blank" rel="noopener noreferrer"><img src={chris} className="footer-logo" alt="ChRIS"/></a>
                 </PageHeaderToolsItem>
             </PageHeaderTools>
         </React.Fragment>
@@ -37,10 +37,10 @@ import {
       className="footer"
       aria-label="Page Footer"
       headerTools={pageToolbar}
-      logo={(<React.Fragment><a 
+      logo={(<a 
         onClick={(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => { history.push('/'); e.preventDefault() }} 
         href="/#"><img src={logo} className="logo" alt="darwinAI" height="300px" width="190px"/>
-        </a></React.Fragment>)}>
+        </a>)}>
     </PageHeader>);
   }
   
