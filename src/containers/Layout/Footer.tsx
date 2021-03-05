fcb15161cdcc28a3cd2304d5df952a587ae3aa8c
Colin Wu <colin.y.wu@gmail.com>
2021-03-05 10:28:50 -0500

    Adjusted content to include ChRIS

--- src/containers/Layout/Footer.tsx
+++ src/containers/Layout/Footer.tsx
@@ -18,8 +18,8 @@ const Footer: React.FC = () => {
         <PageHeaderToolsItem>
           <p className="footer-text">An open-source neural network for disease detection created by&nbsp;
             <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer">DarwinAI</a>,&nbsp;
-            <a href="https://www.redhat.com/en" target="_blank" rel="noopener noreferrer">RedHat</a>, and&nbsp;
-            <a href="https://www.childrenshospital.org/" target="_blank" rel="noopener noreferrer">Boston Children's Hospital</a></p>
+            <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer">ChRIS</a>, and&nbsp;
+            <a href="https://www.redhat.com/en" target="_blank" rel="noopener noreferrer">RedHat</a>&nbsp;</p>
         </PageHeaderToolsItem>
       </PageHeaderTools>
       <PageHeaderTools>
