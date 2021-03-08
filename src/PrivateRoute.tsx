99023659349144b96ab9f0fe08495588187a54eb
Colin Wu <colin.y.wu@gmail.com>
2021-03-08 16:39:01 -0500

    Fixed spacing and quotation marks

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
-import { Redirect, Route } from 'react-router-dom'
-import { AppContext } from './context/context'
+import React, { useContext } from "react"
+import { Redirect, Route } from "react-router-dom"
+import { AppContext } from "./context/context"
 
 interface PrivateRouteProps {
   component: React.FC<any>;
@@ -10,7 +10,7 @@ interface PrivateRouteProps {
 const PrivateRoute: React.FC<PrivateRouteProps> = (props: PrivateRouteProps) => {
   const isLoggedIn = useContext(AppContext).state.user.loggedIn;
 
-  return (isLoggedIn ? (<Route {...props} exact={true} />) : (<Redirect  to="/login"  />));
+  return (isLoggedIn ? (<Route {...props} exact={true} />) : (<Redirect to="/login" />));
 }
 
 export default PrivateRoute
