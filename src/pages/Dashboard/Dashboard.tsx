81b7086083764aa7d1c4b65bb822070b7394a4fd
Colin Wu <colin.y.wu@gmail.com>
2021-03-22 16:19:49 -0400

    Removed unecessary useEffect from Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,22 +1,14 @@
 import { PageSection, PageSectionVariants } from "@patternfly/react-core";
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { CreateAnalysisSection } from "../../components/CreateAnalysis/CreateAnalysis";
 import PastAnalysisTable from "../../components/pastAnalysis/PastAnalysisTable";
 import Wrapper from "../../containers/Layout/PageWrapper";
-import { AnalysisTypes, NotificationActionTypes, StagingDcmImagesTypes } from "../../context/actions/types";
-import { AppContext } from "../../context/context";
-import CreateAnalysisService from "../../services/CreateAnalysisService";
 
 type AllProps = RouteComponentProps;
 
 const DashboardPage: React.FC<AllProps> = () => {
-  const { state: { stagingDcmImages, models }, dispatch } = useContext(AppContext);
-
-  useEffect(() => {
-    document.title = "Analysis - COVID-Net UI";
-    if (stagingDcmImages.length <= 0) return;
-  }, [dispatch, stagingDcmImages, models.ctModel, models.xrayModel]);
+  document.title = "Analysis - COVID-Net UI";
 
   return (
     <Wrapper>
