6f97a999c0e7f672c10c3a82b6e504adae328f78
colinywu <45584743+colinywu@users.noreply.github.com>
2021-03-24 17:04:32 -0400

    Merge pull request #32 from darwinai/CNXC-247
    * Refactored code to create the analysis in CreateAnalysis, instead of Dashboard. Doing this, this resolves the issue of having mixed concerns and a bug of triggering current analyses to re-run when routing back to Dashboard repeatedly. Code now separates concerns and resolves the bug
    
    * Removed unecessary useEffect from Dashboard
    
    * Minor refactor for context in Dashboard
    
    * Fixed indentation and semi-colons, spacing, and document title
    
    * Semi-colons and consistent quotation marks for imports

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,39 +1,18 @@
 import { PageSection, PageSectionVariants } from "@patternfly/react-core";
-import React, { useContext, useEffect } from "react";
+import React, { useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { CreateAnalysisSection } from "../../components/CreateAnalysis/CreateAnalysis";
 import PastAnalysisTable from "../../components/pastAnalysis/PastAnalysisTable";
 import Wrapper from "../../containers/Layout/PageWrapper";
-import { AnalysisTypes, NotificationActionTypes, StagingDcmImagesTypes } from "../../context/actions/types";
-import { AppContext } from "../../context/context";
-import CreateAnalysisService from "../../services/CreateAnalysisService";
 
 type AllProps = RouteComponentProps;
 
 const DashboardPage: React.FC<AllProps> = () => {
-  const { state: { stagingDcmImages, models }, dispatch } = useContext(AppContext);
 
-  useEffect(() => {
+  useEffect(()=> {
     document.title = "Analysis - COVID-Net UI";
-    if (stagingDcmImages.length <= 0) return;
-
-    // Processing the images
-    CreateAnalysisService.analyzeImages(stagingDcmImages, models.xrayModel, models.ctModel) // Passing selected models to Chris_Integration for image analysis
-      .then((notifications) => {
-        dispatch({
-          type: StagingDcmImagesTypes.UpdateStaging,
-          payload: { imgs: [] }
-        })
-        dispatch({
-          type: AnalysisTypes.Update_are_new_imgs_available,
-          payload: { isAvailable: true }
-        });
-        dispatch({
-          type: NotificationActionTypes.SEND,
-          payload: { notifications }
-        })
-      });
-  }, [dispatch, stagingDcmImages, models.ctModel, models.xrayModel]);
+  }, []);
+  
 
   return (
     <Wrapper>
