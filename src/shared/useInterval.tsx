e2dc55779ab75b0df7cf13b6513f74f30af2f6a8
janakitti <janakittiratana@gmail.com>
2021-03-29 14:30:29 -0400

    Fix indentation

--- src/shared/useInterval.tsx
+++ src/shared/useInterval.tsx
@@ -1,25 +1,25 @@
 import { useEffect, useRef } from "react";
 
 const useInterval = (callback: () => void, delay: number): void => {
-    const savedCallback = useRef<() => void>();
+  const savedCallback = useRef<() => void>();
 
-    useEffect(() => {
-        savedCallback.current = callback;
-    }, [callback]);
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-    useEffect(() => {
-        const tick = () => {
-            if (savedCallback.current) {
-                savedCallback.current();
-            }
-        }
+  useEffect(() => {
+    const tick = () => {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
 
-        if (delay) {
-            const id = setInterval(tick, delay);
-            return () => clearInterval(id);
-        }
+    if (delay) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
 
-    }, [delay]);
+  }, [delay]);
 }
 
 export default useInterval;
