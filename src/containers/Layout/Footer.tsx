2ba3597a79181b3bc3f39b72f21dc8288b232500
Colin Wu <colin.y.wu@gmail.com>
2021-04-16 16:13:57 -0400

    Fixed margins for Footer Tools Item

--- src/containers/Layout/Footer.tsx
+++ src/containers/Layout/Footer.tsx
@@ -9,26 +9,26 @@ interface FooterProps {
   isLogin?: boolean
 }
 
-
 const Footer: React.FC<FooterProps> = ({ isLogin }) => {
 
   const pageToolbar = (
     <div className="footer-content">
       <PageHeaderTools>
         <PageHeaderToolsItem>
-          <p className="footer-text">An open-source neural network for disease detection created by&nbsp;
+          <p className="footer-text">An open-source neural network platform for disease detection created by&nbsp;
             <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer">DarwinAI</a>,&nbsp;
-            <a href="https://chrisproject.org/" target="_blank" rel="noopener noreferrer">ChRIS</a>, and&nbsp;
-            <a href="https://redhat.com/chris" target="_blank" rel="noopener noreferrer">RedHat</a>&nbsp;</p>
+            <a href="https://chrisproject.org/" target="_blank" rel="noopener noreferrer">BCH/ChRIS</a>, and&nbsp;
+            <a href="https://redhat.com/chris" target="_blank" rel="noopener noreferrer">Red Hat</a>&nbsp;</p>
         </PageHeaderToolsItem>
       </PageHeaderTools>
+      <span>|</span>
       <PageHeaderTools>
         <PageHeaderToolsItem className="footer-link">
           <p className="footer-text">
-            Find more about&nbsp;
+            Learn more about&nbsp;
             <a href="https://alexswong.github.io/COVID-Net/" target="_blank" rel="noopener noreferrer">COVID-Net</a>
             &nbsp;and&nbsp;
-            <a href="https://github.com/darwinai/covidnet_ui" target="_blank" rel="noopener noreferrer">COVID-Net UI</a>
+            <a href="https://github.com/darwinai/covidnet_ui" target="_blank" rel="noopener noreferrer">the COVID-Net UI</a>
           </p>
         </PageHeaderToolsItem>
       </PageHeaderTools>
