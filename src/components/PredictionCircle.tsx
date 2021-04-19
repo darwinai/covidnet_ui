d5e3cc23b3abcbe187c12feec509ccef4f73cb28
Colin Wu <colin.y.wu@gmail.com>
2021-04-19 14:37:34 -0400

    Fixed indentations

--- src/components/PredictionCircle.tsx
+++ src/components/PredictionCircle.tsx
@@ -7,7 +7,7 @@ interface circleProps {
   isNormal: boolean;
 }
 
-const PredictionCircle: React.FC<circleProps> = ({largeCircle, predictionNumber, isNormal}) => {
+const PredictionCircle: React.FC<circleProps> = ({ largeCircle, predictionNumber, isNormal }) => {
   let divSize = (!largeCircle) ? "80px" : "100px";
   let size = (!largeCircle) ? 120 : 150;
   let thresholds = (isNormal) ? [] : [{ value: 0 }, { value: 90 }];
@@ -15,15 +15,15 @@ const PredictionCircle: React.FC<circleProps> = ({largeCircle, predictionNumber,
   return (
     <div style={{ height: divSize, width: divSize }}>
       <ChartDonutUtilization
-      ariaDesc="Prediction Result of Analysis"
-      constrainToVisibleArea={true}
-      data={{ x: 'Prediction', y: predictionNumber }}
-      height={size}
-      width={size}
-      padding={0}
-      title={`${predictionNumber}%`}
-      thresholds={thresholds}
-       />
+        ariaDesc="Prediction Result of Analysis"
+        constrainToVisibleArea={true}
+        data={{ x: 'Prediction', y: predictionNumber }}
+        height={size}
+        width={size}
+        padding={0}
+        title={`${predictionNumber}%`}
+        thresholds={thresholds}
+      />
     </div>
   )
 }
