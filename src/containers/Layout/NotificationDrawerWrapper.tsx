e588525a47d266b2ad220cd22ddc7c4fafeff99d
Colin Wu <colin.y.wu@gmail.com>
2021-04-20 15:21:32 -0400

    Updated Remove action in notification reducer to perform action of deleting an element, makes is so that the spliced array does not need to be passed before the dispatch

--- src/containers/Layout/NotificationDrawerWrapper.tsx
+++ src/containers/Layout/NotificationDrawerWrapper.tsx
@@ -24,13 +24,13 @@ const NotificationDrawerWrapper: React.FC<NotificationDrawerWrapperProps> = ({ o
   const { state: { notifications }, dispatch } = useContext(AppContext);
 
   const onNotificationRemoval = (index: number) => {
-    let newNotifications: NotificationItem[] = notifications.slice(0);
-    newNotifications.splice(index, 1);
+    // let newNotifications: NotificationItem[] = notifications.slice(0);
+    // newNotifications.splice(index, 1);
 
     dispatch({
       type: NotificationActionTypes.REMOVE,
       payload: {
-        notifications: newNotifications
+        index
       }
     });
   }
