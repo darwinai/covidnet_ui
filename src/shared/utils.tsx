d34a027ac0231a6e63806e2e21451fc330b817d1
Boris Nguyen <dnnguyen@edu.uwaterloo.ca>
2021-04-19 20:16:41 -0400

    Update src/shared/utils.tsx

--- src/shared/utils.tsx
+++ src/shared/utils.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 
 export const formatDate = (dateStr: string): string => {
-  return DateTime.fromISO(dateStr).toFormat('yyyy MM dd');
+  return DateTime.fromISO(dateStr).toFormat("yyyy MM dd");
 }
 
 export const formatGender = (gender: string): string => {
