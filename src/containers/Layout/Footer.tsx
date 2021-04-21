dbac0d64e77b94653e5187dcd6002220e75455f9
Colin Wu <colin.y.wu@gmail.com>
2021-04-21 19:13:05 -0400

    Updated spacing and added spacing for pipe between two text bodies in footer

--- src/containers/Layout/Footer.tsx
+++ src/containers/Layout/Footer.tsx
@@ -18,17 +18,18 @@ const Footer: React.FC<FooterProps> = ({ isLogin }) => {
         <PageHeaderToolsItem>
           <p className="footer-text">An open source clinical decision support UI platform created by&nbsp;
             <a href="https://www.darwinai.com/" target="_blank" rel="noopener noreferrer">DarwinAI</a>,&nbsp;
-            <a href="https://chrisproject.org/" target="_blank" rel="noopener noreferrer">ChRIS</a>, and&nbsp;
-            <a href="https://redhat.com/chris" target="_blank" rel="noopener noreferrer">RedHat</a>&nbsp;</p>
+            <a href="https://chrisproject.org/" target="_blank" rel="noopener noreferrer">BCH/ChRIS</a>, and&nbsp;
+            <a href="https://redhat.com/chris" target="_blank" rel="noopener noreferrer">Red Hat</a>&nbsp;</p>
         </PageHeaderToolsItem>
       </PageHeaderTools>
+      <span>|</span>
       <PageHeaderTools>
         <PageHeaderToolsItem className="footer-link">
           <p className="footer-text">
-            Find more about&nbsp;
+            Learn more about&nbsp;
             <a href="https://alexswong.github.io/COVID-Net/" target="_blank" rel="noopener noreferrer">COVID-Net</a>
             &nbsp;and&nbsp;
-            <a href="https://github.com/darwinai/covidnet_ui" target="_blank" rel="noopener noreferrer">COVID-Net UI</a>
+            <a href="https://github.com/darwinai/covidnet_ui" target="_blank" rel="noopener noreferrer">the COVID-Net UI</a>
           </p>
         </PageHeaderToolsItem>
       </PageHeaderTools>
