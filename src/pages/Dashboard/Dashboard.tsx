a27a189d1587987e4606dca81da53904be13b4a6
Matthew Wang <mslwang2001@gmail.com>
2021-05-12 13:11:47 -0400

    [CNXC-322] Add button and notebox that directs user to CreateAnalysis page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
-import { PageSection, PageSectionVariants } from "@patternfly/react-core";
+import { PageSection, PageSectionVariants, Alert, Button } from "@patternfly/react-core";
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, useHistory } from "react-router-dom";
+import ArrowRightIcon from '@patternfly/react-icons/dist/js/icons/arrow-right-icon';
 import PastAnalysisTable from "../../components/pastAnalysis/PastAnalysisTable";
 import Wrapper from "../../containers/Layout/PageWrapper";
 
@@ -8,11 +9,20 @@ type AllProps = RouteComponentProps;
 
 const DashboardPage: React.FC<AllProps> = () => {
 
+  const history = useHistory();
+
   return (
     <Wrapper>
       <PageSection className="page-body">
-        {/* <PageSection className="section-area" variant={PageSectionVariants.light}>
-        </PageSection> */}
+        <PageSection className="section-area" variant={PageSectionVariants.light}>
+          <Alert isInline variant="warning" title="Patient lookup designated to Create Analysis page">
+            <p>To look up a patient and create a new analysis, please go to the Create Analysis page.</p>
+          </Alert>
+          <Button variant="link" isLarge className="pf-u-pl-0" 
+          onClick={() => history.push('/createAnalysis')}> 
+          Create Analysis <ArrowRightIcon/>
+          </Button>
+        </PageSection>
         <PageSection className="flex-column" variant={PageSectionVariants.light}>
           <PastAnalysisTable></PastAnalysisTable>
         </PageSection>
