76dd7db2ac4edad101548d2c36308ff50f791ca0
Matthew Wang <mslwang2001@gmail.com>
2021-05-14 18:21:33 -0400

    Add loading spinner to patient lookup search

--- src/pages/CreateAnalysisPage/CreateAnalysisPage.tsx
+++ src/pages/CreateAnalysisPage/CreateAnalysisPage.tsx
@@ -1,4 +1,4 @@
-import { PageSection, PageSectionVariants } from "@patternfly/react-core";
+import { PageSection, PageSectionVariants, Spinner } from "@patternfly/react-core";
 import React, { useState } from "react";
 import CreateAnalysisWrapper from "../../components/CreateAnalysis/CreateAnalysisWrapper";
 import PatientLookup from "../../components/PatientLookup";
@@ -9,23 +9,34 @@ import Error from "../../shared/error";
 const CreateAnalysisPage = () => {
   const { state: { dcmImages, createAnalysis: { patientID } } } = React.useContext(AppContext);
   const [hasSearched, setHasSearched] = useState(patientID && patientID !== "");
+  const [isSearching, setIsSearching] = useState(false);
+  let pageSectionContent;
+
+  if(isSearching){
+    pageSectionContent = (
+      <div className="loading">
+        <Spinner size="xl" /> &nbsp; Searching
+      </div>
+    );
+  } else if(dcmImages.allDcmImages.length > 0) {
+    pageSectionContent = (
+      <CreateAnalysisWrapper></CreateAnalysisWrapper>
+    );
+  }else {
+    pageSectionContent = (
+    <Error>{ hasSearched ? "No studies found for MRN #" + patientID : "Search for a patient by entering their MRN above" } </Error>
+  );
+  }
 
   return (
     <Wrapper>
       <PageSection className="page-body">
         <PageSection className="section-area" variant={PageSectionVariants.light}>
-          <PatientLookup setHasSearched={setHasSearched}></PatientLookup>
+          <PatientLookup setHasSearched={setHasSearched} setIsSearching={setIsSearching}></PatientLookup>
         </PageSection>
-          {
-            dcmImages.allDcmImages.length > 0 ?
-              <PageSection variant={PageSectionVariants.light}>
-                <CreateAnalysisWrapper></CreateAnalysisWrapper>
-              </PageSection>
-              :
-              <PageSection className="page-section-no-results" variant={PageSectionVariants.light}>
-                <Error>{ hasSearched ? "No studies found for MRN #" + patientID : "Search for a patient by entering their MRN above" } </Error>
-              </PageSection>
-          }
+          <PageSection variant={PageSectionVariants.light}>
+            {pageSectionContent}
+            </PageSection>
         </PageSection>
     </Wrapper>
   )
