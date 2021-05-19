0db0d583e9c3b75fd5da9e71fdf608875e785e3b
Matthew Wang <mslwang2001@gmail.com>
2021-05-19 15:35:44 -0400

    Remove Duration from imports

--- src/shared/utils.tsx
+++ src/shared/utils.tsx
@@ -1,4 +1,4 @@
-import { DateTime, Duration } from "luxon";
+import { DateTime } from "luxon";
 
 export const formatDate = (dateStr: string): string => {
   return DateTime.fromISO(dateStr).toFormat("yyyy MM dd");
